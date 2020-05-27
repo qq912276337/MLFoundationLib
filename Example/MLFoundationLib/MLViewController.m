ba640c19d8d1870a32fe722a9959458c266586ba
sml <smlsjq@163.com>
2020-05-27 19:51:45 +0800

    initvc

--- Example/MLFoundationLib/MLViewController.m
+++ Example/MLFoundationLib/MLViewController.m
@@ -7,23 +7,93 @@
 //
 
 #import "MLViewController.h"
+#import <Masonry/Masonry.h>
 
-@interface MLViewController ()
+static NSString *kMLVcCellKey = @"kMLVcCellKey";
+
+@interface MLViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@property (nonatomic, strong) NSMutableArray *items;
 
 @end
 
 @implementation MLViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor grayColor];
+    self.title = NSStringFromClass([self class]);
+    
+    [self.view addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        if (@available(iOS 11.0, *)) {
+            make.top.equalTo(self.view.mas_safeAreaLayoutGuideTop);
+            make.bottom.equalTo(self.view.mas_safeAreaLayoutGuideBottom);
+            make.left.equalTo(self.view.mas_safeAreaLayoutGuideLeft);
+            make.right.equalTo(self.view.mas_safeAreaLayoutGuideRight);
+        }else{
+            make.top.equalTo(self.mas_topLayoutGuideBottom);
+            make.bottom.equalTo(self.mas_bottomLayoutGuideTop);
+            make.left.equalTo(self.view);
+            make.right.equalTo(self.view);
+        }
+    }];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UITableView
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return self.items.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSArray *arr = self.items[section];
+    return arr.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kMLVcCellKey forIndexPath:indexPath];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+#pragma mark - Getter and Setter
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+        _tableView.rowHeight = 60;
+        _tableView.tableFooterView = [UIView new];
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kMLVcCellKey];
+        
+        if ([_tableView respondsToSelector:@selector(setSeparatorInset:)]) {
+            [_tableView setSeparatorInset:UIEdgeInsetsZero];
+        }
+        if ([_tableView respondsToSelector:@selector(setLayoutMargins:)]) {
+            [_tableView setLayoutMargins:UIEdgeInsetsZero];
+        }
+        if([_tableView respondsToSelector:@selector(setCellLayoutMarginsFollowReadableWidth:)]) {
+            _tableView.cellLayoutMarginsFollowReadableWidth = NO;
+        }
+    }
+    return _tableView;
+}
+
+- (NSMutableArray *)items {
+    if (!_items) {
+        _items = [[NSMutableArray alloc] init];
+    }
+    return _items;
 }
 
 @end
